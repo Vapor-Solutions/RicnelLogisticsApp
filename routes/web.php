1024c25a88da8f18098662b60924322e615cc02f
Steve Nyanumba <reach@stevenyanumba.com>
2023-05-25 12:36:41 +0300

    Managed to rectify the invoices

--- routes/web.php
+++ routes/web.php
@@ -166,7 +166,7 @@ Route::prefix('admin')->middleware([
             ]);
 
             $date = Carbon::parse(Invoice::find($id)->created_at)->toDateString();
-            return $pdf->download($date . '-Invoice#' . $id . '.pdf');
+            return $pdf->download($date . '- RICNEL - Invoice # ' . $id . '.pdf');
             // return $pdf->stream();
         })->name('admin.invoices.show');
     });
